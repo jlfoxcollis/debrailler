ab8258159e3b12508c28c3487fa7dee2d94dbfcd
James Fox-Collis <jlfoxcollis@gmail.com>
2020-11-05 17:50:28 -0700

    numbers pass

--- test/braille_test.rb
+++ test/braille_test.rb
@@ -10,14 +10,14 @@ class Braille < MiniTest::Test
   def test_numbers
     assert_equal [".0", "00", ".."], @braille.txt["0"]
     assert_equal ["0.", "..", ".."], @braille.txt["1"]
-    assert_equal [".0", "00", ".."], @braille.txt["2"]
-    assert_equal [".0", "00", ".."], @braille.txt["3"]
-    assert_equal [".0", "00", ".."], @braille.txt["4"]
-    assert_equal [".0", "00", ".."], @braille.txt["5"]
-    assert_equal [".0", "00", ".."], @braille.txt["6"]
-    assert_equal [".0", "00", ".."], @braille.txt["7"]
-    assert_equal [".0", "00", ".."], @braille.txt["8"]
-    assert_equal [".0", "00", ".."], @braille.txt["9"]\
+    assert_equal ["0.", "0.", ".."], @braille.txt["2"]
+    assert_equal ["00", "..", ".."], @braille.txt["3"]
+    assert_equal ["00", ".0", ".."], @braille.txt["4"]
+    assert_equal ["0.", ".0", ".."], @braille.txt["5"]
+    assert_equal ["00", "0.", ".."], @braille.txt["6"]
+    assert_equal ["00", "00", ".."], @braille.txt["7"]
+    assert_equal ["0.", "00", ".."], @braille.txt["8"]
+    assert_equal [".0", "0.", ".."], @braille.txt["9"]\
   end
 
 end
