cb9b5dd0e3d875144bda644b5064ad7c1657f7d7
James Fox-Collis <jlfoxcollis@gmail.com>
2020-11-05 18:02:16 -0700

    All letters downcase pass

--- test/braille_test.rb
+++ test/braille_test.rb
@@ -27,8 +27,37 @@ class Braille < MiniTest::Test
     assert_equal ["..", "..", "00"], @braille.txt["-"]
     assert_equal ["..", "00", ".0"], @braille.txt["."]
     assert_equal ["..", "0.", "00"], @braille.txt["?"]
-    assert_equal ["..", "..", ".0"], @braille.txt["Capital"]
+    assert_equal ["..", "..", ".0"], @braille.txt["Caps"]
     assert_equal ["..", "..", ".."], @braille.txt[" "]
+  end
+
+  def test_the_letters
+    assert_equal ["0.", "..", ".."], @braille.txt["a"]
+    assert_equal ["0.", "0.", ".."], @braille.txt["b"]
+    assert_equal ["00", "..", ".."], @braille.txt["c"]
+    assert_equal ["00", ".0", ".."], @braille.txt["d"]
+    assert_equal ["0.", ".0", ".."], @braille.txt["e"]
+    assert_equal ["00", "0.", ".."], @braille.txt["f"]
+    assert_equal ["00", "00", ".."], @braille.txt["g"]
+    assert_equal ["0.", "00", ".."], @braille.txt["h"]
+    assert_equal [".0", "0.", ".."], @braille.txt["i"]
+    assert_equal [".0", "00", ".."], @braille.txt["j"]
+    assert_equal ["0.", "..", "0."], @braille.txt["k"]
+    assert_equal ["0.", "0.", "0."], @braille.txt["l"]
+    assert_equal ["00", "..", "0."], @braille.txt["m"]
+    assert_equal ["00", ".0", "0."], @braille.txt["n"]
+    assert_equal ["0.", ".0", "0."], @braille.txt["o"]
+    assert_equal ["00", "0.", "0."], @braille.txt["p"]
+    assert_equal ["00", "00", "0."], @braille.txt["q"]
+    assert_equal ["0.", "00", "0."], @braille.txt["r"]
+    assert_equal [".0", "0.", "0."], @braille.txt["s"]
+    assert_equal [".0", "00", "0."], @braille.txt["t"]
+    assert_equal ["0.", "..", "00"], @braille.txt["u"]
+    assert_equal ["0.", "0.", "00"], @braille.txt["v"]
+    assert_equal [".0", "00", ".0"], @braille.txt["w"]
+    assert_equal ["00", "..", "00"], @braille.txt["x"]
+    assert_equal ["00", ".0", "00"], @braille.txt["y"]
+    assert_equal ["0.", ".0", "00"], @braille.txt["z"]
   end
 
 end
